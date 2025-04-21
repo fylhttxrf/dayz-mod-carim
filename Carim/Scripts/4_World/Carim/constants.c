95cbfd77567d5636beee2b454a16458bb59ff3e8
Nathaniel Schaaf <schana@users.noreply.github.com>
2025-04-21 02:32:05 -0500

    Remove uppercase files

--- Carim/Scripts/4_World/Carim/constants.c
+++
@@ -1,8 +0,0 @@
-#ifndef CARIM_CarimConstants
-#define CARIM_CarimConstants
-
-static const string CARIM_CHAT_PREFIX = "ᐅ";
-static const int CARIM_60_FPS_INTERVAL_MS = 16;
-static const string CARIM_DELETE = "CARIM_DELETE";
-
-#endif
