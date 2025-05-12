79034a83bb0c523c9db87318db0ebd68a1e2ee1d
Nathaniel Schaaf <schana@users.noreply.github.com>
2025-05-12 09:19:53 -0500

    Fix maxPing bug when set to 0

--- Carim/Carim/scripts/4_world/Carim/model/abc/markers.c
+++ Carim/Carim/scripts/4_world/Carim/model/abc/markers.c
@@ -180,12 +180,22 @@ class CarimModelPartyPings extends CarimModelAbcMarkers {
 
     override void Add(CarimMapMarker mark) {
         int maxPings = CfgGameplayHandler.GetCarimPartyMaxPings();
-        if (maxPings > 0 && markers.Contains(mark.carimPlayerId) && markers.Get(mark.carimPlayerId).Count() >= maxPings) {
-            markers.Get(mark.carimPlayerId).RemoveOrdered(0);
+        bool markersLimited = maxPings >= 0;
+        int countMarkers = 0;
+        if (markers.Contains(mark.carimPlayerId)) {
+            countMarkers = markers.Get(mark.carimPlayerId).Count();
         }
-        mark.CarimSetMarkerText(mark.GetMarkerText() + " " + counter.ToString());
-        ++counter;
-        super.Add(mark);
+        // Remove greater than max markers
+        if (markersLimited && countMarkers >= maxPings) {
+            markers.Get(mark.carimPlayerId).RemoveOrdered(0);
+            --countMarkers;
+        }
+        // Add marker if it would be less than max
+        if (!markersLimited || countMarkers < maxPings) {
+            mark.CarimSetMarkerText(mark.GetMarkerText() + " " + counter.ToString());
+            ++counter;
+            super.Add(mark);
+        }
     }
 }
 
