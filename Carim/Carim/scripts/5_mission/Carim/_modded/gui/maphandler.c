f4cab785a9257d3b8b83b922e18c9198183169c9
Nathaniel Schaaf <schana@users.noreply.github.com>
2025-05-25 10:43:57 -0500

    Update conditional and formatting

--- Carim/Carim/scripts/5_mission/Carim/_modded/gui/maphandler.c
+++ Carim/Carim/scripts/5_mission/Carim/_modded/gui/maphandler.c
@@ -3,7 +3,9 @@ modded class MapHandler {
 
     override bool OnMouseButtonDown(Widget w, int x, int y, int button) {
         if (carimMenuEditMarker && carimMenuEditMarker.visible) {
-            if (MapWidget.Cast(w)) carimMenuEditMarker.Hide();
+            if (w == m_Root) {
+                carimMenuEditMarker.Hide();
+            }
             return true;
         }
         return super.OnMouseButtonDown(w, x, y, button);
